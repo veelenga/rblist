7d46cba0ac94907d910bed365cb4f732878df2ad
Vitalii Elenhaupt <velenhaupt@gmail.com>
2022-03-18 17:25:43 +0200

    Correct gradient

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
     <div className="relative text-center px-1 py-1 bg-black text-neutral-100">
       <a href="/" className="py-4 align-center inline-block">
         <img src="/logo.png" alt="logo" className="h-16 inline-block" />
-        <h1 className="px-4 text-4xl font-bold text-red-500/80 inline-block align-middle">
+        <h1 className="gradient-title px-4 text-4xl font-bold inline-block align-middle">
           Russia Block List
         </h1>
       </a>
