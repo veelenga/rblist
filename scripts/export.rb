e7f823e1be41ac89a8da6c0cfa866fff2bd0e198
Vitalii Elenhaupt <velenhaupt@gmail.com>
2022-03-25 11:17:49 +0200

    Add company db

---
+++ scripts/export.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require 'csv'
+require 'open-uri'
+require 'json'
+
+TAG_MAP = {
+  'consumer products' => 'retail',
+  'consumer cyclical' => 'retail',
+  'consumer defensive' => 'retail',
+  'basic materials' => 'manufacture',
+  'industrials' => 'manufacture',
+  'metals & mining' => 'manufacture',
+  'financial services' => 'finance',
+  'food products' => 'food',
+  'technology' => 'tech',
+  'communication services' => 'communication'
+}.freeze
+
+SUPPORTED_STATUSES = [
+  'Suspension',
+  'Withdrawal'
+].freeze
+
+def download_img(url, name)
+  URI.open(url) do |image|
+    File.open("public/logo/#{name}", 'wb') do |file|
+      file.write(image.read)
+    end
+  end
+end
+
+def logo_url_name(company_name, logo)
+  name, url = logo.split(' ')
+  url = url[1..-2] # remove parenthesis
+  ext = name.split('.').last
+  [url, "#{company_name}.#{ext}"]
+end
+
+items = []
+
+CSV.foreach('scripts/data.csv', headers: true) do |row|
+  name = row['Name']
+  action = row['Action']
+  logo = row['Logo']
+  status = row['Status']
+  website = row['Website URL']
+  instagram = row['Instagram']
+  twitter = row['Twitter']
+  sector = row['Sector']
+
+  next unless SUPPORTED_STATUSES.include?(status)
+
+  url, logo = logo_url_name(name, logo)
+
+  begin
+    #download_img(url, logo)
+  rescue StandardError
+    next
+  end
+
+  next unless logo
+
+  sector&.downcase!
+  tag = TAG_MAP[sector] || sector
+
+  items << {
+    name: name,
+    logo: logo,
+    description: action,
+    status: status,
+    website: website,
+    instagram: instagram,
+    twitter: twitter,
+    tags: [tag].compact
+  }
+end
+
+puts JSON.pretty_generate(items)
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,21 +14,23 @@ function Card(props: IProps) {
     <div className="h-36 w-80 px-2 rounded overflow-hidden shadow-lg">
       <div className="flex">
         <img
-          className="h-20 pt-2 px-4 float-left"
+          className="w-28 h-full pt-2 px-4 float-left"
           src={logo}
           alt="Company logo"
         />
-        <div className="py-1 px-2">
+        <div className="py-1 px-2 h-28">
           <div className="font-bold text-base">{item.name}</div>
           <p className="text-gray-700 text-xs h-14">{item.description}</p>
         </div>
       </div>
 
-      <div className="pt-5 px-1 float-right">
-        {item.tags.map((tag) => (
-          <Tag key={`${item.name}-${tag}`} name={tag} />
-        ))}
-      </div>
+      {item.tags.length > 0 &&
+        <div className="px-1 float-right">
+          {item.tags.map((tag) => (
+            <Tag key={`${item.name}-${tag}`} name={tag} />
+          ))}
+        </div>
+      }
     </div>
   );
 }
